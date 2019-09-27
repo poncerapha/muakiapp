5ddeeddbfd82b0470b859bc2c1b49ca3d08d2ed8
Raphael Fleury <raphaelfleury@BRRJOBITAIM1437.local>
2019-09-27 17:04:16 -0300

    Implementacao navegacao app

--- app/src/main/java/com/example/muakiapp/MainActivity.java
+++ app/src/main/java/com/example/muakiapp/MainActivity.java
@@ -2,9 +2,11 @@ package com.example.muakiapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,10 +14,53 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView numbers = findViewById(R.id.numbers);
+
+        numbers.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                Intent numbersIntent = new Intent(MainActivity.this,NumbersActivity.class);
+                startActivity(numbersIntent);
+            }
+        });
+
+
+        TextView colors = findViewById(R.id.colors);
+
+        colors.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                Intent colorsIntent = new Intent(MainActivity.this,ColorsActivity.class);
+                startActivity(colorsIntent);
+            }
+        });
+
+
+        TextView phrases = findViewById(R.id.phrases);
+
+        phrases.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                Intent phrasesIntent = new Intent(MainActivity.this,PhrasesActivity.class);
+                startActivity(phrasesIntent);
+            }
+        });
+
+
+        TextView family = findViewById(R.id.family);
+
+        family.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                Intent familyIntent= new Intent(MainActivity.this,FamilyActivity.class);
+                startActivity(familyIntent);
+            }
+        });
+
+
+
     }
 
-    public void openNumbersList(View view){
-        Intent i = new Intent(this, NumbersActivity.class);
-        startActivity(i);
-    }
+
 }
