ddcae1bf0ef0abb4c19382c18485ff5d30a78130
Raphael Fleury <raphaelfleury@BRRJOBITAIM1437.local>
2019-09-30 18:36:50 -0300

    Adicionando o up button no app

--- app/src/main/java/com/example/muakiapp/NumbersActivity.java
+++ app/src/main/java/com/example/muakiapp/NumbersActivity.java
@@ -16,6 +16,15 @@ import java.util.ArrayList;
 public class NumbersActivity extends AppCompatActivity {
     MediaPlayer mMediaPlayer;
 
+    private MediaPlayer.OnCompletionListener mCompletionListener = new MediaPlayer.OnCompletionListener(){
+        @Override
+        public void onCompletion(MediaPlayer mediaPlayer){
+            releaseMediaPlayer();
+            Toast.makeText(NumbersActivity.this, "I'm done!", Toast.LENGTH_SHORT).show();
+        }
+
+    };
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -48,9 +57,27 @@ public class NumbersActivity extends AppCompatActivity {
                 Word word = words.get(position);
                 mMediaPlayer = MediaPlayer.create(NumbersActivity.this, word.getmAudioResourceId());
                 mMediaPlayer.start();
+                mMediaPlayer.setOnCompletionListener(mCompletionListener);
             }
         });
 
 
+    }
+
+    /**
+     * Clean up the media player by releasing its resources.
+     */
+    private void releaseMediaPlayer() {
+        // If the media player is not null, then it may be currently playing a sound.
+        if (mMediaPlayer != null) {
+            // Regardless of the current state of the media player, release its resources
+            // because we no longer need it.
+            mMediaPlayer.release();
+
+            // Set the media player back to null. For our code, we've decided that
+            // setting the media player to null is an easy way to tell that the media player
+            // is not configured to play an audio file at the moment.
+            mMediaPlayer = null;
+        }
     }
 }
