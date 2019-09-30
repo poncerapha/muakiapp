fd498d4e04e91e424e404b6e2e6d38fee9db96ae
Raphael Fleury <raphaelfleury@BRRJOBITAIM1437.local>
2019-09-30 14:46:47 -0300

    Adicionando todo o dicionario muaki

--- app/src/main/java/com/example/muakiapp/ColorsActivity.java
+++ app/src/main/java/com/example/muakiapp/ColorsActivity.java
@@ -3,12 +3,38 @@ package com.example.muakiapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class ColorsActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_colors);
+        setContentView(R.layout.activity_numbers);
+
+
+        ArrayList<Word> words = new ArrayList<Word>();
+
+        words.add(new Word("red", "weṭeṭṭi"));
+        words.add(new Word("green", "chokokki"));
+        words.add(new Word("brown", "ṭakaakki"));
+        words.add(new Word("gray", "ṭopoppi"));
+        words.add(new Word("black", "kululli"));
+        words.add(new Word("white", "kelelli"));
+        words.add(new Word("dusty yellow", "ṭopiisә"));
+        words.add(new Word("mustard yellow", "chiwiiṭә"));
+
+
+        WordAdapter adapter = new WordAdapter(this, words);
+
+        ListView listView = findViewById(R.id.list);
+
+        listView.setAdapter(adapter);
+
+
     }
+
+
 }
